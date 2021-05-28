e0b72baaea067ce90f5b9d1d3ee39ea5789da541
sivasonai <ios@trioangle.com>
2021-05-28 19:01:48 +0530

    make it better

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const http = require('http');
 const port = process.env.PORT || 3000;
-
+var aasa = fs.readFileSync(__dirname + '/apple-app-site-association');
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   const msg = 'Hello Node!\n'
-  res.end(msg);
+  res.status(200).send(aasa);
+  res.set('Content-Type', 'application/json');
 });
 
 server.listen(port, () => {
