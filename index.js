b632648c5ae42dc9aea60c73ced0582f12797523
sivasonai <ios@trioangle.com>
2021-05-28 20:00:20 +0530

    make it better

--- index.js
+++ index.js
@@ -1,16 +1,41 @@
 const http = require('http');
 var fs = require('fs');
+var express = require('express');
+const basicRouter = require('./routes');
+const app = express();
+const bodyParser = require('body-parser');
+
 const port = process.env.PORT || 3000;
 var aasa = fs.readFileSync(__dirname + '/apple-app-site-association');
+
+var wiki = require('./wiki.js');
+app.use(bodyParser.urlencoded({extended: true}));
+
+
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   
   const msg = 'Hello Node!\n'
   res.end(msg);
-  res.status(200).send(aasa);
-  res.set('Content-Type', 'application/json');
+
+  // res.set('Content-Type', 'application/json');
 });
 
-server.listen(port, () => {
+// server.listen(port, () => {
+//   console.log(`Server running on http://localhost:${port}/`);
+// });
+app.listen(port, function(){
   console.log(`Server running on http://localhost:${port}/`);
+})
+
+// app.use('/', express.static(__dirname + '/static'));
+app.use('/', basicRouter);
+
+
+// ...
+app.use('/wiki', wiki);
+app.get('/apple-app-site-association', function(req, res, next) {
+  console.log("im called")
+  res.set('Content-Type', 'application/json');
+  res.status(200).send(aasa);
 });
